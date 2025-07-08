e8d4bc38106c18293fdee8392cf0f533cf738552
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-07-08 22:34:56 +0600

    toggle like mutation

--- convex/posts.ts
+++ convex/posts.ts
@@ -43,12 +43,12 @@ export const getFeedPost = query({
     // get all posts
 
     const posts = await ctx.db.query("posts").order("desc").collect();
-    if (posts.length == 0) return [];
+    if (posts.length === 0) return [];
 
     // enhance posts with user data
     const postsWithInfo = await Promise.all(
       posts.map(async (post) => {
-        const postAuthor = await ctx.db.get(post.userId);
+        const postAuthor = (await ctx.db.get(post.userId))!;
 
         const like = await ctx.db
           .query("likes")
@@ -78,5 +78,47 @@ export const getFeedPost = query({
     );
 
     return postsWithInfo;
+  },
+});
+
+export const toggleLike = mutation({
+  args: { postId: v.id("posts") },
+  handler: async (ctx, args) => {
+    const currentUser = await getAuthenticatedUser(ctx);
+
+    const existing = await ctx.db
+      .query("likes")
+      .withIndex("by_user_and_post", (q) =>
+        q.eq("userId", currentUser._id).eq("postId", args.postId)
+      )
+      .first();
+
+    const post = await ctx.db.get(args.postId);
+    if (!post) throw new Error("Post not found");
+
+    if (existing) {
+      // remove like
+      await ctx.db.delete(existing._id);
+      await ctx.db.patch(args.postId, { likes: post.likes - 1 });
+      return false; // unliked
+    } else {
+      // add like
+      await ctx.db.insert("likes", {
+        userId: currentUser._id,
+        postId: args.postId,
+      });
+      await ctx.db.patch(args.postId, { likes: post.likes + 1 });
+
+      // if it's not my post create a notification
+      if (currentUser._id !== post.userId) {
+        await ctx.db.insert("notifications", {
+          receiverId: post.userId,
+          senderId: currentUser._id,
+          type: "like",
+          postId: args.postId,
+        });
+      }
+      return true; // liked
+    }
   },
 });
