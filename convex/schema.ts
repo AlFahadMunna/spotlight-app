1a788180d601bd28e38d7dca27362fad5926c5cc
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-07-08 21:50:52 +0600

    get feed post function defined

--- convex/schema.ts
+++ convex/schema.ts
@@ -53,7 +53,7 @@ export default defineSchema({
 
   bookmarks: defineTable({
     userId: v.id("users"),
-    postId: v.id("users"),
+    postId: v.id("posts"),
   })
     .index("by_user", ["userId"])
     .index("by_post", ["postId"])
